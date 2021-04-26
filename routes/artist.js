35a486b9b9362e058047148429f36921eb856a52
Katarina Orlovska <k.orlovska@posteo.de>
2021-04-26 21:12:57 +0200

    data queries

--- routes/artist.js
+++ routes/artist.js
@@ -12,10 +12,27 @@ router.get('/artist-search', (req, res, next) => {
     title: req.query.q,
     type: 'artist'
   }).then(c => {
-    console.log(c)
+    //console.log(c)
     res.render('artist/artist-results', {
       artists: c.results
     })
   });
+});
+
+router.get('/artist/:id', (req, res, next) => {
+  console.log(req.params.id);
+  dis.getArtistReleases(req.params.id)
+  .then(albums => {
+    //console.log(albums);
+    dis.search({ id: 13714044})
+    .then(id => console.log(id))
+    res.render('artist/album-view', {albums: albums.releases});
+})
+});
+
+router.get('/artist/:id/:master_id', (req,res,next) => {
+
+  dis.getMaster(req.params.master_id)
+  .then(master => console.log(master))
 })
 module.exports = router;
