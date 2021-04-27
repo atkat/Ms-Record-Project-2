c4fdf2031c28cedb38f86ffccad1bec546fd2624
Steffen Leppin <leppinsteffen@gmail.com>
2021-04-27 20:34:00 +0200

    added wishlist/collection

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
+var Discogs = require('disconnect').Client;
+
+var dis = new Discogs({
+  consumerKey: process.env.CONSUMER_KEY,
+  consumerSecret: process.env.CONSUMER_SECRET
+}).database();
 
 const loginCheck = () => {
   return (req, res, next) => {
@@ -16,11 +22,49 @@ router.get("/", (req, res, next) => {
 });
 
 router.get('/profile', loginCheck(), (req, res, next) => {
-  res.render('profile');
+  const collection = req.session.user.records
+  console.log(req.session.user.records);
+  const records = [];
+  let counter = 0
+
+  collection.forEach(recordId => {
+    dis
+      .getRelease(recordId)
+      .then(record => {
+        counter++
+        records.push(record)
+        console.log(records)
+        if (counter === collection.length) {
+          res.render('profile', {
+            records
+          })
+        }
+      })
+  })
 })
 
+
+
 router.get('/wishlist', loginCheck(), (req, res, next) => {
-  res.render('wishlist');
+  const collection = req.session.user.wishList
+  console.log(req.session.user.wishList);
+  const records = [];
+  let counter = 0
+
+  collection.forEach(recordId => {
+    dis
+      .getRelease(recordId)
+      .then(record => {
+        counter++
+        records.push(record)
+        console.log(records)
+        if (counter === collection.length) {
+          res.render('wishlist', {
+            records
+          })
+        }
+      })
+  })
 })
 
 module.exports = router;
