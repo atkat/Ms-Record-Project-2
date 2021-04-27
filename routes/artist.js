383bf7cc96dc1f4f915ee2dc41ca652ede1a2573
Diogo Major <diogocmajor@gmail.com>
2021-04-27 16:38:14 +0200

    wishlist created

--- routes/artist.js
+++ routes/artist.js
@@ -6,16 +6,33 @@ var dis = new Discogs({
   consumerSecret: process.env.CONSUMER_SECRET
 }).database();
 
-
 router.get('/artist-search', (req, res, next) => {
   dis.search({
     title: req.query.q,
     type: 'artist'
   }).then(c => {
-    console.log(c)
+    //console.log(c)
     res.render('artist/artist-results', {
       artists: c.results
     })
   });
-})
+});
+
+router.get('/uri', (req, res, next) => {
+  console.log(req.params.id);
+  dis.getArtistReleases(req.params.id)
+    .then(albums => {
+      console.log(albums)
+      res.render('artist/:id', {
+        albums: albums.releases,
+      })
+    })
+});
+
+// router.get('/artist/:id/:master_id', (req, res, next) => {
+//   dis.getMaster(req.params.master_id)
+//     .then(master => console.log(master))
+// })
+
+
 module.exports = router;
